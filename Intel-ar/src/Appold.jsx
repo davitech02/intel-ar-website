3a3e477c81c32b2c615f652780ad880b9bd302dd
davitech02 <davitech58@gmail.com>
2026-02-19 22:43:26 +0100

    Updated the App.jxs

---
+++ Intel-ar/src/Appold.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "Arial",
+      textAlign: "center"
+    }}>
+      <div>
+        <h1>Website Temporarily Offline</h1>
+        <p>Service temporarily suspended.</p>
+        <p>Please contact support.</p>
+      </div>
+    </div>
+  );
+}
