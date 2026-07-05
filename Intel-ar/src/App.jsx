91683f947f306d75578d906c592dd48b5cc3ae83
davitech02 <davitech58@gmail.com>
2026-07-05 20:54:49 +0100

    Update website

--- Intel-ar/src/App.jsx
+++ Intel-ar/src/App.jsx
@@ -1,49 +1,70 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { LanguageProvider } from './context/LanguageContext';
+// import React, { useEffect } from 'react';
+// import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+// import { LanguageProvider } from './context/LanguageContext';
 
-// Components
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
+// // Components
+// import Navigation from './components/Navigation';
+// import Footer from './components/Footer';
 
-// Pages
-import Home from './pages/Home';
-import ServicesPage from './pages/ServicesPage';
-import AboutPage from './pages/AboutPage';
-import ExpertsPage from './pages/ExpertsPage';
-import ContactPage from './pages/ContactPage';
-import BlogPage from './pages/BlogPage';
-import BlogPost from './pages/BlogPost';
+// // Pages
+// import Home from './pages/Home';
+// import ServicesPage from './pages/ServicesPage';
+// import AboutPage from './pages/AboutPage';
+// import ExpertsPage from './pages/ExpertsPage';
+// import ContactPage from './pages/ContactPage';
+// import BlogPage from './pages/BlogPage';
+// import BlogPost from './pages/BlogPost';
 
-// Scroll To Top Helper (So you don't start at the bottom of a new page)
-function ScrollToTop() {
-  const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-}
+// // Scroll To Top Helper (So you don't start at the bottom of a new page)
+// function ScrollToTop() {
+//   const { pathname } = useLocation();
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+//   return null;
+// }
+
+// function App() {
+//   return (
+//     <LanguageProvider>
+//       <Router>
+//         <ScrollToTop />
+//         <Navigation />
+        
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/services" element={<ServicesPage />} />
+//           <Route path="/about" element={<AboutPage />} />
+//           <Route path="/experts" element={<ExpertsPage />} />
+//           <Route path="/contact" element={<ContactPage />} />
+//           <Route path="/blog" element={<BlogPage />} />
+//           <Route path="/blog/:id" element={<BlogPost />} /> 
+//         </Routes>
+
+//         <Footer />
+//       </Router>
+//     </LanguageProvider>
+//   );
+// }
+
+// export default App;
 
 function App() {
   return (
-    <LanguageProvider>
-      <Router>
-        <ScrollToTop />
-        <Navigation />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/experts" element={<ExpertsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:id" element={<BlogPost />} /> 
-        </Routes>
-
-        <Footer />
-      </Router>
-    </LanguageProvider>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#fff",
+        color: "#000",
+        fontSize: "20px",
+        fontFamily: "Arial, sans-serif",
+      }}
+    >
+      Site Under Construction
+    </div>
   );
 }
 
